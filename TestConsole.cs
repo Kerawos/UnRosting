c98f8edfe4882431ca5227aa8f4321731181644c
Marek.Sowa <marek.sowa@ibm.com>
2020-12-02 12:48:36 +0100

    Add project files.

---
+++ TestConsole.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnRosting
+{
+    class TestConsole
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+            string result = "";
+            while (result == "")
+            {
+                result = Console.ReadLine();
+            }
+        }
+    }
+}
